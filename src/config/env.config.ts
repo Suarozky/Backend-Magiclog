cbbc7885642b7cf34d6db735b61fec286f662064
Suarozky <92611869+Suarozky@users.noreply.github.com>
2025-05-17 13:26:51 -0500

    fix

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -7,9 +7,9 @@ dotenv.config();
 export const EnvConfig = {
   isProduction: process.env.NODE_ENV === 'production',
   port: process.env.PORT ?? 3100,
-  databaseHost: process.env.DATABASE_HOST ?? 'localhost',
-  databasePort: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
+  databaseHost: process.env.DATABASE_HOST ?? 'switchback.proxy.rlwy.net',
+  databasePort: parseInt(process.env.DATABASE_PORT ?? '25696', 10),
   databaseUsername: process.env.DATABASE_USERNAME ?? 'postgres',
-  databasePassword: process.env.DATABASE_PASSWORD ?? 'postgres',
-  databaseName: process.env.DATABASE_NAME ?? 'postgres',
+  databasePassword: process.env.DATABASE_PASSWORD ?? 'UeWcRvzokuheSuyKgKYYFiqllKhucnzI',
+  databaseName: process.env.DATABASE_NAME ?? 'railway',
 };
